ed16fd2b0a657c6cc932748fc8783226b89435e3
Daniel silva do nasciment <daniel.nascimento.1@academico.ifpb.edu.br>
2025-05-13 09:58:36 -0300

    definicao de uma rota e de um cabecalho

--- src/app/daniel/page.js
+++ src/app/daniel/page.js
@@ -1,26 +1,52 @@
 import db from "@/lib/db";
 
 export default async () => {
-    const usuario = await db.query("select * from usuario");
+    const usuario = await db.query("SELECT * FROM usuario");
 
     return (
         <>
             <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
- 
-                <div style={{ width: "100%", height: "84px", backgroundColor: "#ff4791" }}>
-                    
-                </div>
-
-     
-                <div style={{ width: "100%", height: "118px", backgroundColor: "#ffd7d7" }}>
-                    
-                </div>
-            </div>
-
-            <div class="topnav">
-                <input type="text" placeholder="Pesquisar"/>
                 
+                <div 
+                    style={{
+                        width: "100%",
+                        height: "84px",
+                        backgroundColor: "#FF4791", 
+                        display: "flex",
+                        alignItems: "center", 
+                        padding: "0 20px", 
+                    }}
+                >
+                    <div style={{ display: "flex", gap: "10px" }}>
+                        <div
+                            style={{
+                                width: "20px",
+                                height: "20px",
+                                backgroundColor: "#fff",
+                                borderRadius: "50%", // Bola 1
+                            }}
+                        ></div>
+                        <div
+                            style={{
+                                width: "20px",
+                                height: "20px",
+                                backgroundColor: "#fff",
+                                borderRadius: "50%", // Bola 2
+                            }}
+                        ></div>
+                    </div>
                 </div>
+
+                <div 
+                    style={{
+                        width: "100%",
+                        height: "118px",
+                        backgroundColor: "#FFD7D7",
+                    }}
+                >
+                </div>
+
+            </div>
         </>
     );
 };
